8b905d2ba3d8b02bcf6a80c04100ad57f24f8ecd
Alexandr <alexandrlachin989@gmail.com>
2024-09-08 11:46:06 +0300

    redact

--- Command/Build.php
+++ Command/Build.php
@@ -6,7 +6,7 @@ class Build{
     public $OUT_DIR_VENDOR = '';
 
      public function __construct() {
-        
+
        $this->OUT_DIR_VENDOR = $this->search_dir_vendor();
        $this->build_file('pet.sample');
 
@@ -14,20 +14,14 @@ class Build{
     
     function build_file($namesample = ''){
 
-      $sample = file_get_contents("./sample/$namesample.php");
-      file_put_contents($this->OUT_DIR_VENDOR.'pts', $sample );
+      $sample = file_get_contents(__DIR__."/sample/$namesample.php");
+      file_put_contents($this->OUT_DIR_VENDOR.'/pts', $sample );
 
     }
 
 
     function search_dir_vendor(){
-
-        $path = str_replace('\\', DIRECTORY_SEPARATOR, getcwd());
-        if(str_contains($path, 'vendor/')){
-            return explode('vendor/', $path)[0];
-        }else{
-            return './../../../';
-        }
+        return str_replace('\\', DIRECTORY_SEPARATOR, getcwd());
     }
 }
 
