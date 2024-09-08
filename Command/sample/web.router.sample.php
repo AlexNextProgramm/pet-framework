a9814cdc2cf743b43e8037a88694130912b40cfe
Alexandr <alexandrlachin989@gmail.com>
2024-09-08 23:53:21 +0300

    home path

--- Command/sample/web.router.sample.php
+++ Command/sample/web.router.sample.php
@@ -3,12 +3,10 @@
 use Pet\Request\Request;
 use Pet\Router\Router;
 
-Router::get('/home', function(Request $request){ 
+Router::get('/', function(Request $request){ 
     view('home', $request->attribute);
 });
 
-Router::get('/home/{hello}', function(Request $request){ 
-    view('home', $request->parametr);
-});
+
 
 ?>
