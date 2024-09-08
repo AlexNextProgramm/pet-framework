4d4b1cc95dd2861666662261eabc1e1c9fd0b536
Alexandr <alexandrlachin989@gmail.com>
2024-09-08 16:39:46 +0300

    fix

--- Command/sample/pet.sample.php
+++ Command/sample/pet.sample.php
@@ -1,7 +1,7 @@
 <?php
 
 define("ROOT_DIR",  __DIR__);
-
+print_r(__DIR__);
 
 include_once('./vendor/pet/framework/Command/Command.php');
 if($argc == 1) die("Not arguments console \n");
--- Command/sample/web.router.sample.php
+++ Command/sample/web.router.sample.php
@@ -1,6 +1,14 @@
 <?php
 
+use Pet\Request\Request;
 use Pet\Router\Router;
 
-Router::get('/home', function(){ view('home');});
+Router::get('/home', function(Request $request){ 
+    view('home', $request->attribute);
+});
+
+Router::get('/home/{hello}', function(Request $request){ 
+    view('home', $request->parametr);
+});
+
 ?>
