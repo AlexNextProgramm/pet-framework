02836e67ab622cc775307567873f92979b1bace4
Alexandr <alexandrlachin989@gmail.com>
2024-09-21 20:14:01 +0300

    fix

--- DataBase/DB.php
+++ DataBase/DB.php
@@ -11,22 +11,21 @@ abstract class DB{
     private  $db_port = DB_PORT;
     private  $db_user = DB_USER;
     private  $db_password = DB_PASSWORD;
-
     public $DB = null;
 
-    public function conn(){
-        ini_set()
+    public function conn()
+    {
         try{
             $this->DB = new PDO("{$this->db_type}:host={$this->db_host}:{$this->db_port};dbname={$this->db_name}", $this->db_user, $this->db_password);
-            print_r($this->DB);
-        }catch(Exception $e){
-            die($e);
+        }catch(\PDOException $e){
+            die($e->getMessage());
         }
     }
 
-    public function q(){
-
+    public function q($query)
+    {
+       return $this->DB->query($query, PDO::FETCH_ASSOC);
     }
-
+    
 }
 ?>
--- DataBase/Select.php
+++ DataBase/Select.php
@@ -5,6 +5,16 @@ use Pet\DataBase\DB;
 class Select extends DB{
 
 
+    public function __construct()
+    {
+        $this->conn();
+    }
+
+    public function factory($query)
+    {
+        return $this->q($query);
+    }
+    
 
 }
 ?>
