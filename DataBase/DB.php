f9f905d0d0c40a7f1ffe55fe46656d650884c8d0
Alexandr <alexandrlachin989@gmail.com>
2024-09-21 23:21:16 +0300

    fix

--- DataBase/DB.php
+++ DataBase/DB.php
@@ -1,11 +1,13 @@
 <?php
+
 namespace Pet\DataBase;
+
 use PDO;
 use Exception;
 
-abstract class DB{
-    
-    private  $db_type= DB_TYPE;
+abstract class DB {
+
+    private  $db_type = DB_TYPE;
     private  $db_host = DB_HOST;
     private  $db_name = DB_NAME;
     private  $db_port = DB_PORT;
@@ -13,19 +15,18 @@ abstract class DB{
     private  $db_password = DB_PASSWORD;
     public $DB = null;
 
-    public function conn()
-    {
-        try{
+    public function conn() {
+        try {
             $this->DB = new PDO("{$this->db_type}:host={$this->db_host}:{$this->db_port};dbname={$this->db_name}", $this->db_user, $this->db_password);
-        }catch(\PDOException $e){
+        } catch (\PDOException $e) {
             die($e->getMessage());
         }
     }
 
-    public function q($query)
-    {
-       return $this->DB->query($query, PDO::FETCH_ASSOC);
-    }
+
+    public function q($query) {
     
+
+        return $this->DB->query($query, PDO::FETCH_ASSOC);
+    }
 }
-?>
