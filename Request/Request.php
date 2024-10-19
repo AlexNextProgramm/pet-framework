87e9e6f64e506a2abe877e94132708efe02a406b
Alexandr <alexandrlachin989@gmail.com>
2024-10-19 14:36:15 +0300

    Router path

--- Request/Request.php
+++ Request/Request.php
@@ -27,7 +27,8 @@ class Request
     {
         $path = str_contains($_SERVER['REQUEST_URI'], '?') ? explode('?', $_SERVER['REQUEST_URI'])[0] :
             $_SERVER['REQUEST_URI'];
-        return Tools::strRep(strlen($path) - 1, '', $path, '/');
+        return $path != '/'? Tools::strRep(strlen($path) - 1, '', $path, '/'): $path;
+        
     }
 
 
