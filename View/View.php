e88781ec6e5c36cd2f4c0cc7001653f265f183d3
Alexandr <alexandrlachin989@gmail.com>
2024-10-19 16:47:47 +0300

    view

--- View/View.php
+++ View/View.php
@@ -6,10 +6,10 @@ class View
 {
     const DIR_VIEW = PUBLIC_DIR . '/view';
 
-    function open($name, $argument = [])
+    function open($viewName, $argument = [])
     {
-        if (!is_dir(self::DIR_VIEW) || !file_exists(self::DIR_VIEW . "/$name.php")) die("Нет файла или дериктории в view $name.php");
+        if (!is_dir(self::DIR_VIEW) || !file_exists(self::DIR_VIEW . "/$viewName.php")) die("Нет файла или дериктории в view $viewName.php");
         foreach ($argument as $key => $val) ${$key} = $val;
-        include_once self::DIR_VIEW . "/$name.php";
+        include_once self::DIR_VIEW . "/$viewName.php";
     }
 }
