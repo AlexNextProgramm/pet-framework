a0392a56c044426cb5b41d409afd2fb6f357507c
Alexandr <alexandrlachin989@gmail.com>
2024-10-24 22:40:23 +0300

    Cookie  рефакторинг

--- Cookie/Cookie.php
+++ Cookie/Cookie.php
@@ -2,27 +2,55 @@
 namespace Pet\Cookie;
 
 class Cookie{
-
-    public function set($data = [], $path = '/', $secure= false, $http_only = false){
+    
+    /**
+     * set
+     *
+     * @param  array $data
+     * @param  string $path
+     * @param  bool $secure
+     * @param  bool $http_only
+     * @return void
+     */
+    static function set($data = [], $path = '/', $secure= false, $http_only = false){
         foreach($data as $key => $value){
             setcookie($key, $value, time()+(3600*24*30), $path, "", $secure, $http_only);
         }
     }
-
-    public function get($key = ''):null|string{
+    
+    /**
+     * get
+     *
+     * @param  string $key
+     * @return null|string
+     */
+    static function get($key = ''):null|string{
         if(array_key_exists($key, $_COOKIE)){
             return $_COOKIE[$key];
         }
         return null;
     }
-
-    public function httpOnly($data = [], $path = '/'){
+    
+    /**
+     * httpOnly
+     *
+     * @param  array $data
+     * @param  string $path
+     * @return void
+     */
+    static function httpOnly($data = [], $path = '/'){
         ini_set('session.cookie_httponly', 1);
-        $this->set($data, '/', true, true);
+        self::set($data,  $path , true, true);
        
     }
-
-    public function delete(string $name){
+    
+    /**
+     * delete
+     *
+     * @param  string $name
+     * @return void
+     */
+    static function delete(string $name){
         setcookie($name, "", -1, '/');
     }
 }
