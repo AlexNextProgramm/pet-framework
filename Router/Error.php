1c81985d3e384384e2a0d13d5921e6a8032458f8
Alexandr Lachin <alexandrlachin989@gmail.com>
2025-04-27 14:44:45 +0300

    fix

--- Router/Error.php
+++ Router/Error.php
@@ -7,11 +7,11 @@ class Error
     const FORBIDDEN = 403;
     public static $events = [];
 
-    public static function setHttp($code, $massange)
+    public static function setHttp($code, $massange = null)
     {
         http_response_code($code);
         if (key_exists($code, self::$events)) {
-            (new EssenceClass)->open(self::$events[$code], $massange);
+            (new EssenceClass)->open(self::$events[$code], [$massange]);
         }
     }
 }
