df7a5dcd8218a4f1738d0a176615873cc8be9086
Alexandr Lachin <alexandrlachin989@gmail.com>
2025-05-03 20:17:36 +0300

    UI update

--- Frontend/rocet/core/UI.ts
+++ Frontend/rocet/core/UI.ts
@@ -4,7 +4,9 @@ import { Rocet } from "./rocet";
 export class UI{
     private Elements: HTMLElement[] = [];
     public callback: Function = null;
-
+    public events: Array<Function> = [];
+    private renderObserver:Function
+    public countElemets = 0; 
     constructor(id:string) { 
         this.open(id);
     }
@@ -12,17 +14,39 @@ export class UI{
     private open(id: string)
     {
         this.Elements = Array.from(document.querySelectorAll(id));
-        if(this.Elements.length === 0) this.Elements = Array.from(document.querySelectorAll("#" + id))
-        if(this.Elements.length === 0) this.Elements = Array.from(document.querySelectorAll("." + id));
+        if (this.Elements.length === 0) this.Elements = Array.from(document.querySelectorAll("#" + id));
+        if (this.Elements.length === 0) this.Elements = Array.from(document.querySelectorAll("." + id));
+        if (this.Elements.length === 0) { 
+            this.watchElement(id, this)
+        }
     }
 
-    public render(renderFunction:Function|undefined = undefined) {
+    public render(renderFunction: Function | undefined = undefined) {
+        if (this.Elements.length == 0) { 
+            this.renderObserver = renderFunction;
+            return;
+        }
         this.Elements.forEach((element:HTMLElement) => {
             const context = new Rocet(element)
             if (this.callback) { 
                 this.callback(context);
             }
+            context.ExecAfter = this.events;
             context.render(renderFunction);
+        });
+    }
+
+    private watchElement(selector: string, UI:UI) {
+
+        return new Promise((resolve) => {
+            const observer = new MutationObserver(() => {
+                UI.Elements = Array.from(document.querySelectorAll(selector));
+                if (UI.Elements.length != 0) {
+                    console.log(UI.Elements)
+                    UI.render(UI.renderObserver);
+                }
+            });
+            observer.observe(document.body, { childList: true, subtree: true });
         });
     }
 }
