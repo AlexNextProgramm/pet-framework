72a292349431f0f8a1ced119e442dcdf1f2710a7
Alexandr Lachin <alexandrlachin989@gmail.com>
2025-08-11 23:08:01 +0300

    Маска для телефонов

---
+++ Frontend/Tools/Mask.tsx
@@ -0,0 +1,88 @@
+import { $, Rocet } from "@rocet/rocet";
+
+export class Mask {
+    private select: string = '[type=phone]';
+    private sample: string | null = '';
+    private sampleClear: string | null = '';
+    private value: string;
+
+    constructor(selector: string = null) {
+        this.select = selector || this.select;
+    }
+    /**
+     * Model * = replace number
+     * Template = +7(***)-**-**'
+     * 
+     **/
+    public modelNumber(sample: string) {
+        if (sample == '') return console.warn('Mask model not null');
+        this.sample = sample;
+        this.claerInt();
+        this.init();
+    }
+    private init(): void {
+        const $input = $(this.select);
+        if ($input.length == 0) return console.log('Mask elements 0');
+        $input.on('keydown', (ev: KeyboardEvent) => {
+            ev.preventDefault();
+            this.change($(ev.target), ev.key, this)
+        });
+        $input.on('paste', (ev: ClipboardEvent) => {
+            const data: string = String(parseInt(ev.clipboardData.getData('text'))) || '';
+            for (let i = 0; i < data.length; i++) {
+                if (!Number.isNaN(parseInt(this.sampleClear[i]))) {
+                    continue;
+                }
+                this.change($(ev.target), data[i], this);
+            }
+            ev.preventDefault();
+        });
+    }
+
+    private change($target: Rocet, key: any, mask: Mask) {
+        let template: string = $target.attr('data-mask') || mask.sample;
+        if (key == 'Backspace') {
+            let del: string[] = ($target.val() || '').split('');
+            let pattern = ($target.attr('data-mask') || mask.sample).split('');
+            for (let i = del.length - 1; i >= 0; i--) {
+
+                if (!Number.isNaN(parseInt(del[i]))) {
+                    del[i] = '';
+                    if (mask.sample[i] == "*")
+                        pattern[i] = "*";
+                    break;
+                }
+                del[i] = '';
+            }
+            $target.attr('data-mask', pattern.join(''));
+            $target.val(del.join(''));
+        }
+
+        let data = parseInt(key);
+        if (Number.isNaN(data)) return;
+        let regx = new RegExp('([\*]{1,1})', 'i');
+        template = template.replace(regx, String(data));
+
+        $target.attr('data-mask', template);
+        let value = '';
+
+        for (let i = 0; i < template.length; i++) {
+            if (template[i] == '*') {
+                break;
+            }
+            value += template[i];
+        }
+
+        $target.val(value);
+    }
+
+
+    private claerInt() {
+        for (let i = 0; i < this.sample.length; i++) {
+            if (this.sample[i] == '*' || !Number.isNaN(parseInt(this.sample[i]))) {
+                this.sampleClear += this.sample[i];
+                break;
+            }
+        }
+    }
+}
